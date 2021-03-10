5a2cdd91227a6a016ab6265feacec50aaa6443ea
Emanuele Cortesi <ecortesi@TWEDDLE.COM>
2021-03-10 08:50:11 +0100

    add intentionally blank page element

---
+++ src/PhpWord/Writer/Word2007/Element/IntentionallyBlankPage.php
@@ -0,0 +1,238 @@
+<?php
+/**
+ * This file is part of PHPWord - A pure PHP library for reading and writing
+ * word processing documents.
+ *
+ * PHPWord is free software distributed under the terms of the GNU Lesser
+ * General Public License version 3 as published by the Free Software Foundation.
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code. For the full list of
+ * contributors, visit https://github.com/PHPOffice/PHPWord/contributors.
+ *
+ * @see         https://github.com/PHPOffice/PHPWord
+ * @copyright   2010-2018 PHPWord contributors
+ * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
+ */
+
+namespace PhpOffice\PhpWord\Writer\Word2007\Element;
+
+/**
+ * Intentionally blank page element writer
+ *
+ * @since 0.18.0
+ */
+class IntentionallyBlankPage extends Text
+{
+    protected $nTextBreak = 15;
+
+    /**
+     * Write intentionally blank page element.
+     */
+    public function write()
+    {
+        //{ IF { =MOD({ PAGE } ; 2)} = 0 "true" "false" \*MERGEFORMAT }
+
+        $xmlWriter = $this->getXmlWriter();
+        $element = $this->getElement();
+        if (!$element instanceof \PhpOffice\PhpWord\Element\IntentionallyBlankPage) {
+            return;
+        }
+
+        $text = $element->getText();
+
+        $this->startElementP();
+
+        //{
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:fldChar');
+        $xmlWriter->writeAttribute('w:fldCharType', 'begin');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        $xmlWriter->startElement('w:r');
+
+        //IF
+        $xmlWriter->startElement('w:instrText');
+        $xmlWriter->writeAttribute('xml:space', 'preserve');
+        $this->writeText(' IF ');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //{
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:fldChar');
+        $xmlWriter->writeAttribute('w:fldCharType', 'begin');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //=MOD(
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:instrText');
+        $xmlWriter->writeAttribute('xml:space', 'preserve');
+        $this->writeText(' =MOD(');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //{
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:fldChar');
+        $xmlWriter->writeAttribute('w:fldCharType', 'begin');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //PAGE
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:instrText');
+        $xmlWriter->writeAttribute('xml:space', 'preserve');
+        $this->writeText(' PAGE ');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //}
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:fldChar');
+        $xmlWriter->writeAttribute('w:fldCharType', 'end');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //;2)
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:instrText');
+        $xmlWriter->writeAttribute('xml:space', 'preserve');
+        $this->writeText(' ; 2) ');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //}
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:fldChar');
+        $xmlWriter->writeAttribute('w:fldCharType', 'end');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //=
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:instrText');
+        $xmlWriter->writeAttribute('xml:space', 'preserve');
+        $this->writeText(' = ');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //0
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:instrText');
+        $this->writeText('0');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //true
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:instrText');
+        $xmlWriter->writeAttribute('xml:space', 'preserve');
+        $this->writeText(' " ');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        $this->endElementP();
+
+        //text break
+        for ($i=0;$i<$this->nTextBreak;$i++) {
+            $xmlWriter->startElement('w:p');
+            $xmlWriter->endElement();
+        }
+
+        //Text style
+        $xmlWriter->startElement('w:p');
+        $xmlWriter->startElement('w:pPr');
+        $xmlWriter->startElement('w:jc');
+        $xmlWriter->writeAttribute('w:val', 'center');
+        $xmlWriter->endElement();//w:jc
+        $xmlWriter->startElement('w:rPr');
+        $xmlWriter->startElement('w:b');
+        $xmlWriter->endElement();//w:b
+        $xmlWriter->startElement('w:bCs');
+        $xmlWriter->endElement();//w:bCs
+        $xmlWriter->startElement('w:sz');
+        $xmlWriter->writeAttribute('w:val', '32');
+        $xmlWriter->endElement();//w:sz
+        $xmlWriter->startElement('w:szCs');
+        $xmlWriter->writeAttribute('w:val', '32');
+        $xmlWriter->endElement();//w:szCS
+        $xmlWriter->endElement();//w:rPr
+        $xmlWriter->endElement();//w:pPr
+
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:rPr');
+        $xmlWriter->startElement('w:b');
+        $xmlWriter->endElement();//w:b
+        $xmlWriter->startElement('w:bCs');
+        $xmlWriter->endElement();//w:bCs
+        $xmlWriter->startElement('w:sz');
+        $xmlWriter->writeAttribute('w:val', '32');
+        $xmlWriter->endElement();//w:sz
+        $xmlWriter->startElement('w:szCs');
+        $xmlWriter->writeAttribute('w:val', '32');
+        $xmlWriter->endElement();//w:szCS
+        $xmlWriter->endElement();//w:rPr
+
+        //Text
+        $xmlWriter->startElement('w:instrText');
+        $this->writeText($text);
+        $xmlWriter->endElement();//w:instrText
+
+        $xmlWriter->endElement();//w:r
+        $xmlWriter->endElement();//w:p
+
+        //page break
+        $xmlWriter->startElement('w:p');
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:br');
+        $xmlWriter->writeAttribute('w:type', 'page');
+        $xmlWriter->endElement(); // w:br
+        $xmlWriter->endElement(); // w:r
+        $xmlWriter->endElement(); // w:p
+
+        $this->startElementP();
+
+        $xmlWriter->startElement('w:pPr');
+        $xmlWriter->startElement('w:rPr');
+        $xmlWriter->startElement('w:noProof');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:lastRenderedPageBreak');
+        $xmlWriter->endElement();
+        $xmlWriter->startElement('w:instrText');
+        $this->writeText('"');
+        $xmlWriter->endElement();//w:instrText
+        $xmlWriter->endElement();//w:r
+
+        //false
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:instrText');
+        $xmlWriter->writeAttribute('xml:space', 'preserve');
+        $this->writeText(' ""');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //\* MERGEFORMAT
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:instrText');
+        $xmlWriter->writeAttribute('xml:space', 'preserve');
+        $this->writeText(' \\* MERGEFORMAT ');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        //}
+        $xmlWriter->startElement('w:r');
+        $xmlWriter->startElement('w:fldChar');
+        $xmlWriter->writeAttribute('w:fldCharType', 'end');
+        $xmlWriter->endElement();
+        $xmlWriter->endElement();
+
+        $this->endElementP(); // w:p
+    }
+}
